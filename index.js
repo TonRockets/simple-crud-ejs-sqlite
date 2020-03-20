068c3c2d6d19d85068fe6e6e39a2e425464be4d8
W. Pereira Santos <w.pereira.santos@avanade.com>
2020-03-20 19:54:29 -0300

    atualização do path views

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), {
     Promise
 })
 
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: true }))
