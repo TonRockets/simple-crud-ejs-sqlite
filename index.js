6833ee9ec7c4461ee34f54be91b9c010cccec37d
W. Pereira Santos <w.pereira.santos@avanade.com>
2020-03-20 05:14:25 -0300

    Inserindo o path para a leitura do css

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'views','public')))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), {
