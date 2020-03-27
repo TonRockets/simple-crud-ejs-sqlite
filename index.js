ad371703dc53ee168d7fa2a97e7b8a29ea21a588
W. Pereira Santos <w.pereira.santos@avanade.com>
2020-03-27 14:06:59 -0300

    Atualização das páginas e index

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), {
 
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', async (req, res) => {
